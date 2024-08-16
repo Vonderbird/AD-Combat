6b05c1fd7138ff4902735425a8b4ebc659c102b2
Vonderbird <VahidRanandeh@gmail.com>
2024-08-15 20:23:12 -0400

    Delete button changes color to red done

--- My project/Assets/Resources/Prefabs/Assets Folder/DeleteButtonController.cs
+++ My project/Assets/Resources/Prefabs/Assets Folder/DeleteButtonController.cs
@@ -3,17 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
+
+
+
 public class DeleteButtonController : MonoBehaviour
 {
     public Button deleteButton;
+    private bool deleteModeActive = false;
+
+    private Image deleteButtonImage;
 
     void Start()
     {
         deleteButton.onClick.AddListener(ToggleDeleteMode);
+        deleteButtonImage = deleteButton.GetComponent<Image>();
     }
 
     void ToggleDeleteMode()
     {
-        MaterialHoverSwitch.deleteMode = !MaterialHoverSwitch.deleteMode;
+        deleteModeActive = !deleteModeActive;
+        MaterialHoverSwitch.deleteMode = deleteModeActive;
+
+        // Change the color of the delete button's image to red when delete mode is active, and back to normal when it's not
+        deleteButtonImage.color = deleteModeActive ? Color.red : Color.white;
+    }
+
+    public void ResetDeleteButton()
+    {
+        deleteModeActive = false;
+        MaterialHoverSwitch.deleteMode = false;
+
+        // Reset the delete button color to normal
+        deleteButtonImage.color = Color.white;
     }
 }
+
