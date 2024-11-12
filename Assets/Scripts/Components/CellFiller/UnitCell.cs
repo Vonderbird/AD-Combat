233042c25eefccd54fb3678bd7ecd88037ee0d75
Fardin Rastakhiz <fardin.rastakhiz@gmail.com>
2024-11-12 21:00:30 +0330

    Update cell selection logic

--- Assets/Scripts/Components/CellFiller/UnitCell.cs
+++ Assets/Scripts/Components/CellFiller/UnitCell.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using Unity.VisualScripting.YamlDotNet.Core.Tokens;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
@@ -14,10 +15,18 @@ namespace ADC.UnitCreation
     {
         public enum CellState
         {
-            UnselectedPointerOver,
-            UnselectedPointerOut,
-            SelectedPointerOver,
-            SelectedPointerOut
+            UnselectedUnfilled,
+            UnselectedFilled,
+            SelectedUnfilled,
+            SelectedFilled
+        }
+
+        public enum PointerActionType
+        {
+            None,
+            Additive,
+            Selective,
+            Deletive
         }
 
         [SerializeField] private Renderer cellRenderer;
@@ -29,33 +38,9 @@ namespace ADC.UnitCreation
         [SerializeField] private Color defaultEmptyColor = Color.white;
         [SerializeField] private Color defaultFilledColor = Color.gray;
 
-        private CellState State
-        {
-            get => cellState;
-            set
-            {
-                if (value == cellState) return;
-
-                cellState = value;
-                switch (value)
-                {
-                    case CellState.UnselectedPointerOver:
-                        cellRenderer.materials[materialId].color = Color.green;
-                        break;
-                    case CellState.UnselectedPointerOut:
-                        cellRenderer.materials[materialId].color = IsFilled ? defaultFilledColor : defaultEmptyColor;
-                        break;
-                    case CellState.SelectedPointerOver:
-                        cellRenderer.materials[materialId].color = Color.cyan;
-                        break;
-                    case CellState.SelectedPointerOut:
-                        cellRenderer.materials[materialId].color = Color.magenta;
-                        break;
-                    default:
-                        throw new ArgumentOutOfRangeException(nameof(value), value, null);
-                }
-            } 
-        }
+        private CellState cellState;
+        public bool IsFilled { get; private set; } = false;
+        public bool IsCellSelected { get; private set; } = false;
 
         private static int _incrementalId;
         public int CellId { get; } = _incrementalId++;
@@ -73,20 +58,98 @@ namespace ADC.UnitCreation
         public bool IsBuildingSelected { get; set; }
         public DeleteButton DeleteButton { get; set; }
 
-        public bool IsFilled => isFilled;
-
-        private bool isFilled = false;
-
-
         void Awake()
         {
             cellEventArgs = new CellEventArgs(CellId);
             if (!cellRenderer)
                 cellRenderer = GetComponentInChildren<Renderer>();
 
-            cellRenderer.materials[materialId].color = IsFilled ? defaultFilledColor : defaultEmptyColor;
-            State = CellState.SelectedPointerOut;
+            UpdateCellState(false, false, PointerActionType.None);
         }
+
+        #region Set state and color
+        private void UpdateCellState(bool isFilled, bool isSelected, PointerActionType pointerActionType)
+        {
+            //if (IsCellSelected == isSelected && IsFilled == isFilled) return;
+            IsCellSelected = isSelected;
+            IsFilled = isFilled;
+            cellState = isFilled
+                ? (isSelected ? CellState.SelectedFilled : CellState.UnselectedFilled)
+                : (isSelected ? CellState.SelectedUnfilled : CellState.UnselectedUnfilled);
+            switch (pointerActionType)
+            {
+                case PointerActionType.None:
+                    UpdateUnHoveredColor();
+                    break;
+                case PointerActionType.Additive:
+                    UpdateHoveredAdditiveColor();
+                    break;
+                case PointerActionType.Selective:
+                    UpdateHoveredSelectiveColor();
+                    break;
+                case PointerActionType.Deletive:
+                    UpdateHoveredDeletiveColor();
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(pointerActionType), pointerActionType, null);
+            }
+        }
+
+        private void UpdateHoveredAdditiveColor()
+        {
+            Color newColor = cellState switch
+            {
+                CellState.UnselectedUnfilled => Color.green,
+                CellState.UnselectedFilled => Color.magenta,
+                CellState.SelectedUnfilled => Color.green,
+                CellState.SelectedFilled => Color.magenta,
+                _ => defaultEmptyColor
+            };
+
+            cellRenderer.materials[materialId].color = newColor;
+        }
+        private void UpdateHoveredSelectiveColor()
+        {
+            Color newColor = cellState switch
+            {
+                CellState.UnselectedUnfilled => defaultEmptyColor,
+                CellState.UnselectedFilled => Color.cyan,
+                CellState.SelectedUnfilled => defaultEmptyColor,
+                CellState.SelectedFilled => Color.blue,
+                _ => defaultEmptyColor
+            };
+
+            cellRenderer.materials[materialId].color = newColor;
+        }
+        private void UpdateHoveredDeletiveColor()
+        {
+            Color newColor = cellState switch
+            {
+                CellState.UnselectedUnfilled => defaultEmptyColor,
+                CellState.UnselectedFilled => Color.red,
+                CellState.SelectedUnfilled => defaultEmptyColor,
+                CellState.SelectedFilled => Color.red,
+                _ => defaultEmptyColor
+            };
+
+            cellRenderer.materials[materialId].color = newColor;
+        }
+        private void UpdateUnHoveredColor()
+        {
+            Color newColor = cellState switch
+            {
+                CellState.UnselectedUnfilled => defaultEmptyColor,
+                CellState.UnselectedFilled => defaultFilledColor,
+                CellState.SelectedUnfilled => defaultEmptyColor,
+                CellState.SelectedFilled => Color.black,
+                _ => defaultEmptyColor
+            };
+
+            cellRenderer.materials[materialId].color = newColor;
+        }
+
+
+        #endregion
 
         public void CreateDecoObject(IUnit unitPrefab, ParticleSystemGroup spawnParticle,
             ParticleSystemGroup deleteParticle, Vector3 position, float scaleFactor)
@@ -108,8 +171,9 @@ namespace ADC.UnitCreation
             if (SpawnParticle)
                 SpawnParticle = null;
 
-            if (!isFilled) return;
-            isFilled = false;
+            if (!IsFilled) return;
+            IsFilled = false;
+            IsCellSelected = false;
             if (hoverIsEnable)
                 cellRenderer.materials[materialId].color = Color.green;
 
@@ -129,7 +193,6 @@ namespace ADC.UnitCreation
         }
 
         private WaitForSeconds deleteDelay = new(1.5f);
-        private CellState cellState;
 
         IEnumerator DeleteDecoWithDelay(GameObject tempObject)
         {
@@ -141,35 +204,40 @@ namespace ADC.UnitCreation
 
         public void OnCellAdditiveEntered()
         {
-            State = IsFilled ? CellState.SelectedPointerOut : CellState.UnselectedPointerOut;
+            UpdateCellState(IsFilled, IsCellSelected, PointerActionType.Additive);
             //cellRenderer.materials[materialId].color = Color.green; //decoObject == null ? Color.green : Color.gray;
         }
 
         public void OnCellDeletionEntered()
         {
-            State = IsFilled ? CellState.UnselectedPointerOut : CellState.SelectedPointerOver;
+            UpdateCellState(IsFilled, IsCellSelected, PointerActionType.Deletive);
+            //cellState = IsFilled ? CellState.UnselectedPointerOut : CellState.SelectedPointerOver;
             //cellRenderer.materials[materialId].color = decoObject == null ? Color.gray : Color.green;
         }
 
+        public void OnCellUnSelect()
+        {
+            IsCellSelected = false;
+            UpdateCellState(IsFilled, IsCellSelected, PointerActionType.None);
+        }
         public void OnCellExit()
         {
-            State = IsFilled? CellState.UnselectedPointerOut : CellState.SelectedPointerOut;
+            UpdateCellState(IsFilled, IsCellSelected, PointerActionType.None);
+            //cellState = IsFilled? CellState.UnselectedPointerOut : CellState.SelectedPointerOut;
             //cellRenderer.materials[materialId].color = IsFilled ? defaultFilledColor : defaultEmptyColor;
         }
         public void OnCellSelected()
         {
-            State = CellState.SelectedPointerOver;
+            IsCellSelected = true;
+            UpdateCellState(IsFilled, IsCellSelected, PointerActionType.Selective);
+            //cellState = CellState.SelectedPointerOver;
             //cellRenderer.materials[materialId].color = Color.magenta; 
-        }
-        public void OnCellDeselected()
-        {
-            State = CellState.UnselectedPointerOut;
-            //cellRenderer.materials[materialId].color = Color.gray ;
         }
         public void OnCellAdditiveClicked()
         {
-            isFilled = true;
-            State = CellState.SelectedPointerOver;
+            IsFilled = true;
+            UpdateCellState(IsFilled, IsCellSelected, PointerActionType.Additive);
+            //cellState = CellState.SelectedPointerOver;
             //cellRenderer.materials[materialId].color = Color.red;
         }
 
@@ -262,7 +330,7 @@ namespace ADC.UnitCreation
 
         public void OnCellUnitSpawned()
         {
-            if (!isFilled) return;
+            if (!IsFilled) return;
             if (!SpawnParticle) return;
             var tempParticle = Instantiate(SpawnParticle, transform.position + Vector3.up * 0.25f, Quaternion.identity,
                 transform);
