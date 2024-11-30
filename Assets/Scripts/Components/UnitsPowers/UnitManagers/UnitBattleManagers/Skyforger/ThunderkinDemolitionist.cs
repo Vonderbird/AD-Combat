0edf57669df573f279bfafb7831f284591aed45b
Fardin Rastakhiz <fardin.rastakhiz@gmail.com>
2024-11-30 20:29:56 +0330

    Set Update mode on thunderkin demolitionist, resolve some bugs, resolve the problem with unit rotations

--- Assets/Scripts/Components/UnitsPowers/UnitManagers/UnitBattleManagers/Skyforger/ThunderkinDemolitionist.cs
+++ Assets/Scripts/Components/UnitsPowers/UnitManagers/UnitBattleManagers/Skyforger/ThunderkinDemolitionist.cs
@@ -1,19 +1,66 @@
 using ADC.API;
+using RTSEngine.EntityComponent;
+using System;
+using System.Linq;
 using UnityEngine;
 
 namespace ADC
 {
+    public enum DemolitionistMode
+    {
+        Demolitionist,
+        Grenade,
+        EMP
+    }
+
+    [Serializable]
+    public class UnitAttackMode
+    {
+        [SerializeField] private bool isActive;
+        [SerializeField] private string name;
+        [SerializeField] private UnitSpecs specs;
+        [SerializeField] private UnitAttack unitAttack;
+
+        public bool IsActive => isActive;
+        public string Name => name;
+        public UnitSpecs Specs => specs;
+        public UnitAttack Attack => unitAttack;
+    }
+
     public class ThunderkinDemolitionist : UnitBattleManager
     {
-        [SerializeField] private SimpleUnitUpdateInfo updateInfo;
+        //public override List<ISpecialAbility> SpecialAbilities { get; protected set; }
+        [SerializeField] private string activeMode;
+        public string ActiveMode { get => activeMode; private set => activeMode = value; }
+
+        [SerializeField] private UnitAttackMode[] modes;
+
+        private FreeMultiModelUpdateInfo updateInfo;
 
         public override IUnitUpdateInfo UpdateInfo
         {
             get
             {
-                updateInfo.OnUpdateAction ??= OnUnitUpdate;
+                if (updateInfo == null || updateInfo.OnUpdateAction == null)
+                {
+                    updateInfo = new FreeMultiModelUpdateInfo
+                    {
+                        OnUpdateAction = OnUpdateMode,
+                        Modes = modes.Select(m => m.Name).ToArray()
+                    };
+                }
+                updateInfo.ActiveMode = string.IsNullOrEmpty(ActiveMode) ? modes.First(m => m.IsActive).Name : ActiveMode;
                 return updateInfo;
             }
+        }
+        private void OnUpdateMode(string mode)
+        {
+            var modeInfo = modes.FirstOrDefault(m => m.Name == ActiveMode);
+            //modeInfo?.Attack.SetActive(false, false);
+            ActiveMode = mode;
+            //modeInfo = modes.FirstOrDefault(m => m.Name == ActiveMode);
+            //modeInfo?.Attack.SetActive(true, true);
+            modeInfo?.Specs.Update(modeInfo.Specs);
         }
 
         //protected override void Awake()
@@ -25,6 +72,13 @@ namespace ADC
         //    };
         //    base.Awake();
         //}
+        protected override void GetInfoFromUnitCell()
+        {
+            if (CellUnit is ThunderkinDemolitionist fh)
+            {
+                OnUpdateMode(fh.ActiveMode);
+            }
+        }
 
         public override void Accept(IUnitManagerVisitor managerVisitor)
         {
