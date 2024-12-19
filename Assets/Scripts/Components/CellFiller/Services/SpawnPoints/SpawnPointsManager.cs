ba2d7971e136544dbc5fefa139dbb8b6886c05d7
Fardin Rastakhiz <fardin.rastakhiz@gmail.com>
2024-12-20 03:25:53 +0330

    Update battleground, update nav mesh, add spawn point manager, resolve some bugs

---
+++ Assets/Scripts/Components/CellFiller/Services/SpawnPoints/SpawnPointsManager.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using ADC.UnitCreation;
+using RTSEngine.Determinism;
+using UnityEngine;
+
+namespace ADC
+{
+    [Serializable]
+    public class SpawnPointsManager
+    {
+        private CellFillerComponent fillerComponent;
+        [SerializeField] private SpawnPoint[] spawnPoints;
+        private List<int> freePoints;
+
+        private System.Random rnd = new ();
+
+        private WaitForSeconds waitForGateRelease = new(4.2f);
+
+        public void OnInit(CellFillerComponent fillerComponent)
+        {
+            this.fillerComponent = fillerComponent;
+            freePoints = Enumerable.Range(0, spawnPoints.Length).ToList();
+        }
+        public SpawnPoint GetNextPoint()
+        {
+            if (freePoints.Count == 0) return null;
+            var randId = rnd.Next(0, freePoints.Count);
+            var drawPoint = freePoints[randId];
+            freePoints.RemoveAt(randId);
+            this.fillerComponent.StartCoroutine(FreeUpGateTimer(drawPoint));
+            return spawnPoints[drawPoint];
+        }
+
+        IEnumerator FreeUpGateTimer(int pointToRelease)
+        {
+            yield return waitForGateRelease;
+            freePoints.Add(pointToRelease);
+        }
+    }
+}
