a35f2fe87124120580b182c34f216cbaef4669e2
Fardin Rastakhiz <fardin.rastakhiz@gmail.com>
2025-01-13 16:44:57 +0330

    Update special abilities and VFXs, protective inspiration, shield dome vfx

--- Assets/API/ShaderControls/FollowerVfxArgs.cs
+++ Assets/API/ShaderControls/FollowerVfxArgs.cs
@@ -2,13 +2,22 @@ using UnityEngine;
 
 namespace ADC.API
 {
-    public class FollowerVfxArgs : ParticleArgs
+    public struct FollowerVfxArgs : ParticleArgs
     {
+        public FollowerVfxArgs(Transform transform, bool autoCalculateOffset = true, Vector3 positionOffset = default, Vector3 rotationOffset = default, Vector3 scaleOffset = default)
+        {
+            AutoCalculateOffset = autoCalculateOffset;
+            PositionOffset = positionOffset;
+            RotationOffset = rotationOffset;
+            ScaleOffset = scaleOffset;
+            Transform = transform;
+        }
+
         public Transform Transform { get; set; }
-        public bool AutoCalculateOffset { get; set; } = true;
-        public Vector3 PositionOffset { get; set; } = default;
-        public Vector3 RotationOffset { get; set; } = default;
-        public Vector3 ScaleOffset { get; set; } = default;
+        public bool AutoCalculateOffset { get; set; }
+        public Vector3 PositionOffset { get; set; }
+        public Vector3 RotationOffset { get; set; }
+        public Vector3 ScaleOffset { get; set; }
 
     }
 }
