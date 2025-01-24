90b8e098f99b8d7cee7456141c3eecc138ac2a76
Fardin Rastakhiz <fardin.rastakhiz@gmail.com>
2025-01-25 00:36:58 +0330

    Start poison system

---
+++ Assets/Scripts/Components/PoisonSystem/ExtendedHoverHealthBar.cs
@@ -0,0 +1,19 @@
+using RTSEngine.Cameras;
+using RTSEngine.Entities;
+using RTSEngine.Event;
+using RTSEngine.UI;
+using UnityEngine;
+using RTSEngine;
+
+//namespace ADC
+//{
+namespace ADC
+{
+    // Attached to the UI canvas that holds the hover health bar, handles displaying the hover health bar at the main camera.
+    [RequireComponent(typeof(Canvas))]
+    public class ExtendedHoverHealthBar : HoverHealthBar
+    {
+
+    }
+}
+//}
