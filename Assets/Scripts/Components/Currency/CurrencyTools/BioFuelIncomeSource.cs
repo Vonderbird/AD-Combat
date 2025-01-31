e0b7480b3ac2558f4cc9ad72548a1995faf30204
Fardin Rastakhiz <fardin.rastakhiz@gmail.com>
2025-02-01 02:09:06 +0330

    Update and refactoring economy system

--- Assets/Scripts/Components/Currency/CurrencyTools/BioFuelIncomeSource.cs
+++ Assets/Scripts/Components/Currency/CurrencyTools/BioFuelIncomeSource.cs
@@ -1,24 +1,26 @@
 using ADC.API;
 using UnityEngine;
+using Zenject;
 
 namespace ADC.Currencies
 {
+    
     public class BiofuelIncomeSource : IncomeSource
     {
         private readonly Biofuel paymentAmount;
         public override decimal PaymentAmount => paymentAmount.Value;
-        public BiofuelIncomeSource(Biofuel paymentAmount, int factionId) :
-            base(factionId)
+
+        [Inject]
+        public BiofuelIncomeSource(IWaveTimer waveTimer, IEconomySystem economySystem, Biofuel paymentAmount, int factionId) :
+            base(waveTimer, economySystem, factionId)
         {
             this.paymentAmount = paymentAmount;
         }
 
-
-
         protected override void Update()
         {
             Debug.Log($"Update with income value: {PaymentAmount}");
-            EconomySystem.Instance[factionId].Deposit(paymentAmount);
+            economySystem[factionId].Deposit(paymentAmount);
         }
     }
 }
